69277c91bc96c73b66f4e127ffdc1f48ba1e9ca3
Fuad Rustamzadeh <rustam.fuad@gmail.com>
2018-10-23 13:59:44 +0400

    some style edits

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component }from "react";
 import axios from "axios";
 let apiKey = "dfacccedf3a35b1589c0ce509793bcae";
 
@@ -44,10 +44,20 @@ getData = () =>{
 
   render() {
     return (
+        <div className="container">
    <div className="card">
    <h1>The weather in Baku is</h1>
-        <h2>{this.state.temp}</h2>
+        <h2 className="number">{this.state.temp}</h2>
+
       </div>
+<div className="sun">
+        <div className="icon-sun" style={{float: "left"}}>
+    <div className="sun-burst" style={{backgroundColor: "#FFCC33"}}></div>
+    <div className="sun-core-outer"></div>
+    <div className="sun-core-inner" style={{backgroundColor: "#FFCC33"}}></div>
+  </div>
+       </div>
+        </div>
     );
   }
 }
